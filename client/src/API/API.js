fa15889c47cb3d085a57825bd513d8d5af50bc17
GONZdev <grgsalamanca@gmail.com>
2021-05-24 06:07:51 +0100

    changing api calls from client

--- client/src/API/API.js
+++ client/src/API/API.js
@@ -9,20 +9,19 @@ const headers = {
                     }
             }
 
-const url = "/api";
 
-export const fetchPost = (postId) => axios.get(`${url}/post/${postId}`);
-export const fetchPosts = (collection, query) => axios.post(`${url}/posts/`, {collection, query});
-export const getProfile = (username) => axios.get(`${url}/get-user/${username}`, headers);
-export const search = (query) => axios.get(`${url}/search/${query}`, headers);
+export const fetchPost = (postId) => axios.get(`/api/post/${postId}`);
+export const fetchPosts = (collection, query) => axios.post("/api/posts/", {collection, query});
+export const getProfile = (username) => axios.get(`/api/get-user/${username}`, headers);
+export const search = (query) => axios.get(`/api/search/${query}`, headers);
 // export const isAuth = (refreshToken) => axios.post(`${url}/is-auth`, refreshToken, )
-export const uploadImage = (image) => axios.post(`${url}/upload-image`, image, headers)
-export const createPost = (newPost) => axios.post(`${url}/create-post`, newPost, { 'Content-Type': 'multipart/form-data' });
-export const signup = (credentials) => axios.post(`${url}/signup`, credentials, headers);
-export const login = (credentials) => axios.post(`${url}/login`, credentials, headers);
-export const getlogin = () => axios.get(`${url}/login`, headers);
-export const logout = (userId) => axios.post(`${url}/logout`, userId, headers);
-export const editProfile = (id, profileData) => axios.post(`${url}/edit-profile/${id}`, profileData); 
-export const likePost = (id) => axios.patch(`${url}/${id}/likePost`);
-export const updatePost = (id, updatedPost) => axios.patch(`${url}/${id}`, updatedPost);
-export const deletePost = (id, userId) => axios.delete(`${url}/delete-post/${id}/${userId}`);
+export const uploadImage = (image) => axios.post("/api/upload-image", image, headers)
+export const createPost = (newPost) => axios.post("/api/create-post", newPost, { 'Content-Type': 'multipart/form-data' });
+export const signup = (credentials) => axios.post("/api/signup", credentials, headers);
+export const login = (credentials) => axios.post("/apilogin", credentials, headers);
+export const getlogin = () => axios.get("/api/login", headers);
+export const logout = (userId) => axios.post("/api/logout", userId, headers);
+export const editProfile = (id, profileData) => axios.post(`/api/edit-profile/${id}`, profileData);
+export const likePost = (id) => axios.patch(`/api/${id}/likePost`);
+export const updatePost = (id, updatedPost) => axios.patch(`/api/${id}`, updatedPost);
+export const deletePost = (id, userId) => axios.delete(`/api/delete-post/${id}/${userId}`);
